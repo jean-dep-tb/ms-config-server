11abf703c73cdae7ac7cd2f46b58c3a07fd77679
sistemas@gmail.com <59699335+jean-dep-tb@users.noreply.github.com>
2020-01-13 19:30:55 -0500

    primara carga server config
    primara carga server config

---
+++ src/test/java/spring/boot/webflux/ms/config/app/SpringBootWebfluxMsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.webflux.ms.config.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxMsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
